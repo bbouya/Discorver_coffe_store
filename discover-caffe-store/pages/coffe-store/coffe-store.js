daeb27e2dae58c0fb07db4d625bf0ddadeb853f5
bbouya <52068209+bbouya@users.noreply.github.com>
2021-12-01 15:49:29 +0100

    Coffe-store
    add folder caffee store

---
+++ discover-caffe-store/pages/coffe-store/coffe-store.js
@@ -0,0 +1,12 @@
+
+
+const CoffeeStore = () => {
+
+    return <div>Coffe Store opage rah 5dama</div>
+}
+
+
+
+
+
+export default CoffeeStore;
--- discover-caffe-store/pages/index.js
+++ discover-caffe-store/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Coffe Connisseur</title>
+        <title>Coffe Connoisseur</title>
        
         <link rel = 'icon' href = '/favion.ico'/>
       </Head>
